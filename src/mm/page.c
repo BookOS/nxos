32b0693abd8bf97ad6a38d2887e8e20e7aeaa074
hzc1998 <2323168280@qq.com>
2022-02-18 17:49:07 +0800

    feat(page): add spin lock

--- src/mm/page.c
+++ src/mm/page.c
@@ -13,8 +13,11 @@
 #include <mm/page.h>
 #include <utils/log.h>
 #include <xbook/debug.h>
+#include <sched/spin.h>
 
 NX_PRIVATE NX_BuddySystem *BuddySystemArray[NX_PAGE_ZONE_NR]; 
+
+NX_PRIVATE NX_Spin buddyLock[NX_PAGE_ZONE_NR];
 
 /**
  * Init buddy memory allocator
@@ -24,40 +27,71 @@ NX_PUBLIC void NX_PageInitZone(NX_PageZone zone, void *mem, NX_USize size)
     NX_ASSERT(zone >= NX_PAGE_ZONE_NORMAL && zone < NX_PAGE_ZONE_NR && size > 0);
     BuddySystemArray[zone] = NX_BuddyCreate(mem, size);
     NX_ASSERT(BuddySystemArray[zone] != NX_NULL);
+    NX_SpinInit(&buddyLock[zone]);
 }
 
 NX_PUBLIC void *NX_PageAllocInZone(NX_PageZone zone, NX_USize count)
 {
     NX_ASSERT(zone >= NX_PAGE_ZONE_NORMAL && zone < NX_PAGE_ZONE_NR && count > 0);
-    return NX_BuddyAllocPage(BuddySystemArray[zone], count);
+    void *addr;
+    NX_UArch level;
+    NX_SpinLockIRQ(&buddyLock[zone], &level);
+    addr = NX_BuddyAllocPage(BuddySystemArray[zone], count);
+    NX_SpinUnlockIRQ(&buddyLock[zone], level);
+    return addr;
 }
 
 NX_PUBLIC NX_Error NX_PageFreeInZone(NX_PageZone zone, void *ptr)
 {
     NX_ASSERT(zone >= NX_PAGE_ZONE_NORMAL && zone < NX_PAGE_ZONE_NR && ptr != NX_NULL);
-    return NX_BuddyFreePage(BuddySystemArray[zone], ptr);
+    NX_Error err;
+    NX_UArch level;
+    NX_SpinLockIRQ(&buddyLock[zone], &level);
+    err = NX_BuddyFreePage(BuddySystemArray[zone], ptr);
+    NX_SpinUnlockIRQ(&buddyLock[zone], level);
+    return err;
 }
 
 NX_PUBLIC NX_Error NX_PageIncreaseInZone(NX_PageZone zone, void *ptr)
 {
     NX_ASSERT(zone >= NX_PAGE_ZONE_NORMAL && zone < NX_PAGE_ZONE_NR && ptr != NX_NULL);
-    return NX_BuddyIncreasePage(BuddySystemArray[zone], ptr);
+    NX_Error err;
+    NX_UArch level;
+    NX_SpinLockIRQ(&buddyLock[zone], &level);
+    err = NX_BuddyIncreasePage(BuddySystemArray[zone], ptr);
+    NX_SpinUnlockIRQ(&buddyLock[zone], level);
+    return err;
 }
 
 NX_PUBLIC void *NX_PageZoneGetBase(NX_PageZone zone)
 {
     NX_ASSERT(zone >= NX_PAGE_ZONE_NORMAL && zone < NX_PAGE_ZONE_NR);
-    return BuddySystemArray[zone]->pageStart;
+    void *addr;
+    NX_UArch level;
+    NX_SpinLockIRQ(&buddyLock[zone], &level);
+    addr = BuddySystemArray[zone]->pageStart;
+    NX_SpinUnlockIRQ(&buddyLock[zone], level);
+    return addr;
 }
 
 NX_PUBLIC NX_USize NX_PageZoneGetPages(NX_PageZone zone)
 {
     NX_ASSERT(zone >= NX_PAGE_ZONE_NORMAL && zone < NX_PAGE_ZONE_NR);
-    return (BuddySystemArray[zone]->maxPFN + 1);
+    NX_USize size;
+    NX_UArch level;
+    NX_SpinLockIRQ(&buddyLock[zone], &level);
+    size = (BuddySystemArray[zone]->maxPFN + 1);
+    NX_SpinUnlockIRQ(&buddyLock[zone], level);
+    return size;
 }
 
 NX_PUBLIC void *NX_PageZoneGetBuddySystem(NX_PageZone zone)
 {
     NX_ASSERT(zone >= NX_PAGE_ZONE_NORMAL && zone < NX_PAGE_ZONE_NR);
-    return BuddySystemArray[zone];
+    void *addr;
+    NX_UArch level;
+    NX_SpinLockIRQ(&buddyLock[zone], &level);
+    addr = BuddySystemArray[zone];
+    NX_SpinUnlockIRQ(&buddyLock[zone], level);
+    return addr;
 }
