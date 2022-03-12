8959aad5445ca737191db2bb818a88a8d607f979
hzc1998 <2323168280@qq.com>
2022-03-13 01:50:24 +0800

    fix(script): bug on setup.sh set env var

--- setup.sh
+++ setup.sh
@@ -16,15 +16,15 @@
 # example: source setup.sh                       # x86-i386
 # example: source setup.sh riscv64-qemu_riscv64  # riscv64-qemu_riscv64
 
-def_target="unknown" 
-
 if [ -z $1 ]
 then
     def_target="x86-i386" # default target is x86-i386
+else
+    def_target=$1
 fi
 
 echo "Set environment for NXOS."
-export NXOS_SRC_DIR=$NXOS_ROOT_DIR/src
+export NXOS_SRC_DIR=$(pwd)/src
 
 echo "[SRC DIR  ]" $NXOS_SRC_DIR
 echo "[PLAFORM  ]" $def_target
